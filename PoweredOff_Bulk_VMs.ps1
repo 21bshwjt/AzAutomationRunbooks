093faee4141fb5f082e07da47eaa003643a42d3d
Biswajit Biswas <42672713+21bshwjt@users.noreply.github.com>
2023-11-14 15:03:03 +0530

    Create PoweredOff_Bulk_VMs.ps1

---
+++ PoweredOff_Bulk_VMs.ps1
@@ -0,0 +1,29 @@
+# Azure DevOps
+<#
+# Runon : Azure 
+# Hybrid Worker : N/A
+#>
+
+# Get the Azure Automation connection object
+$connection = Get-AutomationConnection -Name "<az_aaa_connection>"
+
+# Connect to Azure using the connection object
+Try {
+    Connect-AzAccount -ServicePrincipal `
+        -Tenant $connection.TenantID `
+        -ApplicationId $connection.ApplicationID `
+        -CertificateThumbprint $connection.CertificateThumbprint | Out-Null
+}    
+catch {
+    Write-Error -Message $_.Exception
+    throw $_.Exception
+}
+
+# Set Subscription
+Set-AzContext -SubscriptionId "SubID"
+
+# Create RG Array
+$RGArray = @("RG1", "RG2", "RG3")
+
+# Poweroff the VMs
+$RGArray | ForEach-Object -Process {Get-AzVm -ResourceGroupName $PSItem | Stop-AzVM -Force -AsJob -Verbose}
